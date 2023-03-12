5f80dfb3bac471cbfc1d7822f8fdbb2a7ba6609c
wendecoder <wenonezra258@gmail.com>
2023-03-12 19:23:51 +0300

    corrected fetching link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://my-express-app.netlify.app/.netlify/functions/server/contact", {
+    let response = await fetch("https://my-express-backend.netlify.app/.netlify/functions/server/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
